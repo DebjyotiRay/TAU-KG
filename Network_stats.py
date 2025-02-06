4708668c6d110d949f465563e208b14e48a99b9e
Debjyoti Ray <33850567+DebjyotiRay@users.noreply.github.com>
2025-02-06 16:21:53 +0530

    Update Network_stats.py

--- Network_stats.py
+++ Network_stats.py
@@ -9,7 +9,7 @@ from plotly.subplots import make_subplots
 from scipy.stats import pearsonr
 
 from Network_explorer import *
-from advanced_analyzer import *
+from Advanced_analyzer import *
 from basic_analyzer import *
 class NetworkAnalyzer:
     def __init__(self, nodes_data, edges_data):
